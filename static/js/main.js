449ab9b6916cc8d91836997e964ec661699769a2
MichalPokojny <>
2023-05-05 19:42:53 +0000

    implement top banner change messages effect

---
+++ static/js/main.js
@@ -0,0 +1,20 @@
+// Display banner messages with a fade effect and rotate through a list of messages
+$(document).ready(function() {
+    // Array of banner messages to display
+    var messages = ['Free delivery on orders over €60 !', 'Special offer: Free gift with every purchase!']; 
+    var index = 0;
+
+    // Set a timer to display the banner messages every 5s
+    setInterval(function() {
+        $('.banner-text').fadeOut(function() {
+            $(this).text(messages[index]);
+        }).fadeIn();
+
+        index++;
+
+        if (index === messages.length) {
+            index = 0;
+        }
+    }, 5000);
+});
+
