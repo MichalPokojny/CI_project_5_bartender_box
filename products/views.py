f550e69b29e8cc2027440735d4509c0c60e7b789
MichalPokojny <>
2023-05-24 14:30:56 +0000

    Add product functionality

--- products/views.py
+++ products/views.py
@@ -28,13 +28,13 @@ def all_products(request):
             if sortkey == 'category':
                 sortkey = 'category__name'
             if 'direction' in request.GET:
-                direction = request.GET["direction"]
-                if direction == "desc":
-                    sortkey = f"-{sortkey}"
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
                 products = products.order_by(sortkey)
 
-        if "category" in request.GET:
-            categories = request.GET["category"].split(",")
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
 
@@ -49,7 +49,7 @@ def all_products(request):
                 name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
-    current_sorting = f"{sort}_{direction}"
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
@@ -78,26 +78,26 @@ def product_detail(request, product_id):
 def add_product(request):
     """Add a product to the store"""
     if not request.user.is_superuser:
-        messages.error(request, "Sorry, only store owners can do that.")
-        return redirect(reverse("home"))
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
 
-    if request.method == "POST":
+    if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
             product = form.save()
-            messages.success(request, "Successfully added product!")
-            return redirect(reverse("product_detail", args=[product.id]))
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(
                 request,
-                "Failed to add product. Please ensure the form is valid."
+                'Failed to add product. Please ensure the form is valid.'
             )
     else:
         form = ProductForm()
 
-    template = "products/add_product.html"
+    template = 'products/add_product.html'
     context = {
-        "form": form,
+        'form': form,
     }
 
-    return render(request, template, context)    
+    return render(request, template, context)
