714d4a8e91a576a6152355b26df6de0cfce281a8
MichalPokojny <>
2023-05-25 12:20:20 +0000

    deployment to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn bartender_box.wsgi:application
--- bartender_box/settings.py
+++ bartender_box/settings.py
@@ -18,7 +18,8 @@ SECRET_KEY = 'django-insecure-up8^s6!4qr#rh=o7l=%bg_=)$w1snww+_62@q@_lh7ux&$@__q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-michalpokojny-ci-project-1rc3p7gzfc.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['https://bartener-box.herokuapp.com', 'localhost']
+# 8000-michalpokojny-ci-project-1rc3p7gzfc.us2.codeanyapp.com
 
 
 # Application definition
@@ -110,12 +111,18 @@ WSGI_APPLICATION = 'bartender_box.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 
 # Password validation
