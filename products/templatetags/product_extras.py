349ddfbf04049bdc4e2a22ca0aef09caa538fa43
MichalPokojny <>
2023-06-03 16:56:51 +0000

    add comments

--- products/templatetags/product_extras.py
+++ products/templatetags/product_extras.py
@@ -5,6 +5,9 @@ register = template.Library()
 @register.simple_tag
 def get_user_rating(product, user):
     try:
+        # Retrieve the rating for the given product and user
+        # If the rating exists, return its value
         return product.ratings.get(user=user).value
     except product.ratings.model.DoesNotExist:
+        # If the rating does not exist, return None
         return None
