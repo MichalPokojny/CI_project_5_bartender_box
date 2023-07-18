6307796fc6badee654c522121d09c3fbed7de7a7
MichalPokojny <>
2023-07-18 19:15:35 +0000

    add wishlist items to the admin panel and fix email verification with updated password value

--- bartender_box/settings.py
+++ bartender_box/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['bartender-box.herokuapp.com', '8000-michalpokojny-ci-project-1rc3p7gzfc.us2.codeanyapp.com']
 
--- wishlist/admin.py
+++ wishlist/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import WishlistItem
 
-# Register your models here.
+
+class WishlistItemAdmin(admin.ModelAdmin):
+    list_display = ['user', 'product', 'added_at']
+    list_filter = ['user', 'added_at']
+    search_fields = ['user__username', 'product__name']
+
+admin.site.register(WishlistItem, WishlistItemAdmin)
